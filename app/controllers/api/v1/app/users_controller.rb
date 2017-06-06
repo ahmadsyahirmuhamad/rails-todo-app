01b68cd84df63062a7c9c81ea15a46ff4c7b3cfc
ahmadsyahirmuhamad <ahmadsyahirmuhamad@gmail.com>
2017-06-06 22:45:33 +0800

    user update controller

--- app/controllers/api/v1/app/users_controller.rb
+++ app/controllers/api/v1/app/users_controller.rb
@@ -10,6 +10,25 @@ class Api::V1::App::UsersController < Api::V1::App::BaseController
       }, status: 200)
   end
 
+  def update
+    if current_user.update_attributes(session_params)
+      response = {
+        token: current_user.generate_auth_token,
+        users: current_user.attributes,
+        message: "User Update Success",
+        error: false
+      }
+    else
+      response = {
+        token: "",
+        users: current_user.attributes,
+        message: "User Update Failed",
+        error: true
+      }
+    end
+    render( json: response, status: 200)
+  end
+
   def session_params
     params.require(:user).permit(:email, :password, :first_name, :last_name)
   end
