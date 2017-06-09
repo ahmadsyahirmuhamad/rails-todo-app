bd7ab485af672a04d2ca52a69457b52c1238fe78
ahmadsyahirmuhamad <ahmadsyahirmuhamad@gmail.com>
2017-06-10 03:44:35 +0800

    tweak

--- spec/controllers/api/app/v1/todos_controller_spec.rb
+++ spec/controllers/api/app/v1/todos_controller_spec.rb
@@ -12,16 +12,18 @@ describe Api::App::V1::TodosController do
 
     context 'POST #create' do
       it 'with valid attributes' do
-        params = { todo: { title: "title", description: "description" } }
+        params = { todo: build(:todo, user_id: @user.id).attributes }
         post :create, params
         expect(response.status).to eql(200)
+        expect(@user.todos.count).to eql(2)
         expect(JSON.parse(response.body)['error']).to eql(false)
       end
 
       it 'with invalid attributes' do
-        params = { todo: { title: "", description: "" } }
+        params = { todo: build(:todo, title: "", description: "", user_id: @user.id).attributes }
         post :create, params
         expect(response.status).to eql(200)
+        expect(@user.todos.count).to eql(1)
         expect(JSON.parse(response.body)['error']).to eql(true)
       end
     end
@@ -32,6 +34,7 @@ describe Api::App::V1::TodosController do
         put :update, params
         @todo.reload
         expect(response.status).to eql(200)
+        expect(@user.todos.count).to eql(1)
         expect(JSON.parse(response.body)['error']).to eql(false)
         expect(JSON.parse(response.body)['todo']['title']).to eql(@todo.title)
         expect(JSON.parse(response.body)['todo']['description']).to eql(@todo.description)
@@ -42,6 +45,7 @@ describe Api::App::V1::TodosController do
         put :update, params
         @todo.reload
         expect(response.status).to eql(200)
+        expect(@user.todos.count).to eql(1)
         expect(JSON.parse(response.body)['error']).to eql(true)
         expect(JSON.parse(response.body)['todo']['title']).to_not eql(@todo.title)
         expect(JSON.parse(response.body)['todo']['description']).to_not eql(@todo.description)
@@ -50,12 +54,11 @@ describe Api::App::V1::TodosController do
 
     context 'DELETE #destroy' do
       it 'with valid attributes' do
-        @count = Todo.count
         params = { id: @todo.id }
         delete :destroy, params
         expect(response.status).to eql(200)
         expect(JSON.parse(response.body)['error']).to eql(false)
-        expect(@count - 1).to eql(Todo.count)
+        expect(@user.todos.count).to eql(0)
       end
     end
 
